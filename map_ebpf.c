8817911c96ffe20ee1b5cd64df82fba332078948
Fenix125 <149311187+Fenix125@users.noreply.github.com>
2025-03-22 19:34:00 +0200

    simple hashmap in kernel
    clang -O2 -target bpf -g -c map_ebpf.c -o map_ebpf.o
    
    sudo ip link set enp0s1 xdp obj map_ebpf.o sec xdp
    
    sudo bpftool map show

--- map_ebpf.c
+++ map_ebpf.c
@@ -3,14 +3,25 @@
 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, __u32);   // IPv4 address
+    __type(value, __u64);
     __uint(max_entries, 1024);
-    __type(key, __be32); 
-    __type(value, __u32);
 } ip_map SEC(".maps");
 
 SEC("xdp")
-int xdp_prog(struct __sk_buff *skb) {
+int xdp_prog(struct xdp_md *ctx){
+    __u32 key = 1111;
+    __u64 *value;
+
+    value = bpf_map_lookup_elem(&ip_map, &key);
+    if (!value) {
+        __u64 init_value = 1;
+        bpf_map_update_elem(&ip_map, &key, &init_value, BPF_ANY);
+    } else {
+        (*value)++;
+        bpf_map_update_elem(&ip_map, &key, value, BPF_ANY);
+    }
     return XDP_PASS;
 }
 
-char _license[] SEC("license") = "GPL";
+char _license[] SEC("license") = "MIT";
