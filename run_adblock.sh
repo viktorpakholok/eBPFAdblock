e8284dca771d50ba1e69f7748e102ff52c5dbad3
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2025-05-13 13:27:08 +0300

    Updated shell script

--- run_adblock.sh
+++ run_adblock.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# hardcoded IP handling
-echo "Building and attaching program..."
-make
+
+echo "Installing dependencies..."
+
 make install
 
-echo "Adding hardcoded IP to block..."
-sudo bin/manage add IP_TO_BLOCK 1
 
 if [ -f all_ips.txt ]; then
+    make
     sudo python3 map_ip_adder.py all_ips.txt add
 else
     echo "File all_ips.txt not found. Skipping."
 fi
-
 
 
 # domains handling
@@ -23,8 +21,5 @@ python3 ./domain_parser.py
 echo "Running DNS matching"
 sudo python3 dns_matching.py -d "all_domains.txt"
 
-
-echo "Detaching and cleaning up..."
-make clean
 
 echo "Success."
